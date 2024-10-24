8b0014249045cf69a5700dc872e43380a30956c6
zjhken <zjhken@outlook.com>
2024-10-24 15:13:25 +0800

    add access log

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,8 @@
+use std::time::{Duration, Instant};
+
 use anyhow_ext::Result;
 use tide::{Middleware, Next, Request};
-use tracing::debug;
+use tracing::{debug, error_span, info, info_span, warn, warn_span, Instrument};
 
 pub fn init_http_server_blocking() -> Result<()> {
 	let mut app = tide::new();
@@ -18,7 +20,6 @@ pub fn init_http_server_blocking() -> Result<()> {
 }
 
 struct AuthMiddleware;
-
 #[tide::utils::async_trait]
 impl<State: Clone + Send + Sync + 'static> Middleware<State> for AuthMiddleware {
 	async fn handle(&self, mut req: Request<State>, next: Next<'_, State>) -> tide::Result {
@@ -29,5 +30,52 @@ impl<State: Clone + Send + Sync + 'static> Middleware<State> for AuthMiddleware 
 
 		// res.insert_header("request-number", count.to_string());
 		Ok(res)
+	}
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct AccessLogMiddleware;
+impl AccessLogMiddleware {
+	async fn log<'a, State: Clone + Send + Sync + 'static>(
+		&'a self,
+		ctx: Request<State>,
+		next: Next<'a, State>,
+	) -> tide::Result {
+		let path = ctx.url().path().to_owned();
+		let method = ctx.method();
+
+		Ok(async {
+			let start = Instant::now();
+			let response = next.run(ctx).await;
+			let duration = start.elapsed();
+			let status = response.status();
+
+			info_span!("Response", http.code = status as u16, http.duration = ?duration).in_scope(
+				|| {
+					if status.is_server_error() {
+						let span = error_span!(
+							"internal error",
+							detail = tracing::field::Empty,
+							error = tracing::field::Empty
+						);
+						if let Some(err) = response.error() {
+							span.record("error", tracing::field::display(err));
+							span.record("detail", tracing::field::debug(err));
+						}
+					} else if status.is_client_error() {
+						warn_span!("client error").in_scope(|| warn!("sent"));
+					} else {
+						info!("sent")
+					}
+				},
+			);
+			response
+		}
+		.instrument({
+			let span = info_span!("request", req_id = tracing::field::Empty, method = %method, path = %path);
+			span.record("req_id", rusty_ulid::Ulid::generate().to_string());
+			span
+		})
+		.await)
 	}
 }
